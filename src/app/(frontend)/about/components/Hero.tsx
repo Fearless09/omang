bb9a43e1f340d58e90ccd496800eb98c85f6016d
Fearless09 <toheebopeyemi9@gmail.com>
2025-10-06 17:49:28 +0100

    Font

--- src/app/(frontend)/about/components/Hero.tsx
+++ src/app/(frontend)/about/components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
           "relative aspect-[2.49/2.78] w-[250px] shrink-0 rounded-3xl",
           "before:absolute before:inset-0 before:-z-[1] before:rotate-15 before:rounded-3xl before:bg-gradient-to-b before:from-[#F0B8BF] before:to-[#CEC7DB]",
         )}
-        data-aos="zoom-in-down"
+        data-aos="zoom-out"
       >
         <Image
           alt="Omang Thomas"
